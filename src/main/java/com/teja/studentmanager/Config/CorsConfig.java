6af6fc8038a94dda0809c4e1da591e24099e2cae
Teja <valluriteja5@gmail.com>
2026-06-11 10:08:02 +0530

    final commit

--- src/main/java/com/teja/studentmanager/Config/CorsConfig.java
+++ src/main/java/com/teja/studentmanager/Config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
                                 "https://student-management-frontend-lr06lbsof.vercel.app",
-                                "https://student-management-frontend-lac.vercel.app/"
+                                "https://student-management-frontend-lac.vercel.app/",
+                                "https://student-management-frontend-lr06lbsof.vercel.app/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
